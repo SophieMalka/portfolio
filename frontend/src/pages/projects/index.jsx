acd2215bb5f1fe29a46b199896b6791afc6dcd7d
SophieMalka <s.malka@live.fr>
2023-06-04 14:03:14 +0200

    Title projects page

--- frontend/src/pages/projects/index.jsx
+++ frontend/src/pages/projects/index.jsx
@@ -3,7 +3,12 @@ import './index.css';
 function Projects() {
     return (
         <section id='projects' className='projects'>
-            
+            <div className='projects-container'>
+                <h2 className='projects-title'>
+                    <span className='projects-title-first'>Mes projets</span>
+                    <span className='projects-title-sec'>Découvrez mes compétences en visualisant mes réalisations.</span>
+                </h2>
+            </div>
         </section>
     )
 }
