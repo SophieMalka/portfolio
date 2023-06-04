b781ad255e8bd17fe89ee1e3d9ae33948c856cae
SophieMalka <s.malka@live.fr>
2023-06-04 14:02:30 +0200

    Modif router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Router from './components/router';
+import Admin from './pages/admin';
 import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router />
+      <Routes>
+        <Route path='/' element={<Router />} />
+        <Route path='/admin2103' element={<Admin />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
