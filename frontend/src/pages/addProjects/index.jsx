53db1965f2ce18a37575c81557200214cdf2cbe4
SophieMalka <s.malka@live.fr>
2023-06-07 09:13:46 +0200

    form component

--- frontend/src/pages/addProjects/index.jsx
+++ frontend/src/pages/addProjects/index.jsx
@@ -1,25 +1,31 @@
 import './index.css';
+import Form from '../../components/form';
 
 function AddProjects() {
-function handleSubmit() {
-  const formData = new FormData();
-  formData.append('imgUrl', document.getElementById('imgUrl').files[0]);
-  formData.append('title', document.getElementById('title').value);
-  formData.append('description', document.getElementById('description').value);
-  formData.append('link', document.getElementById('link').value);
+  function handleSubmit() {
+    const formData = new FormData();
+    formData.append('imgUrl', document.getElementById('imgUrl').files[0]);
+    formData.append('title', document.getElementById('title').value);
+    formData.append('description', document.getElementById('description').value);
+    formData.append('link', document.getElementById('link').value);
 
-  fetch('http://localhost:3001/api/projects', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => {
-      // Traitement des données après l'envoi réussi
-      console.log(response)
+    const form = document.querySelector('form');
+
+    fetch('http://localhost:3001/api/projects', {
+      method: 'POST',
+      body: formData,
     })
-    .catch(error => {
-      // Gestion des erreurs lors de l'envoi
-    });
-}
+      .then(response => {
+        // Traitement des données après l'envoi réussi
+        console.log(response)
+        alert('Projet créé !');
+        form.reset();
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+  
   if (sessionStorage.getItem("token")) {
     return (
       <section id='projects' className='projects'>
@@ -27,17 +33,10 @@ function handleSubmit() {
           <h2 className='projects-title'>
             <span className='projects-title-first'>Ajouter un projet</span>
           </h2>
-          <form className='form-add-work'>
-            <label htmlFor='imgUrl'>Image</label>
-            <input type='file' name='imgUrl' id='imgUrl'></input>
-            <label htmlFor='title'>Titre</label>
-            <input type='text' name='title' id='title'></input>
-            <label htmlFor='description'>Description</label>
-            <input type='text' name='description' id='description'></input>
-            <label htmlFor='link'>Lien</label>
-            <input type='text' name='link' id='link'></input>
-            <button type='button' onClick={handleSubmit}>Envoyer</button>
-          </form>
+          <Form
+            classForm={'form-add-word'}
+            functionForm={handleSubmit}
+          />
         </div>
       </section>
     )
