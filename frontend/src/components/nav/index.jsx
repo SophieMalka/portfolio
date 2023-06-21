699fdafdb6790c6eefc4a1e22cac6d09931bed36
SophieMalka <s.malka@live.fr>
2023-06-21 11:06:19 +0200

    css mobile

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,17 +1,38 @@
+import React, { useState } from 'react';
 import './index.css';
 import { HashLink } from 'react-router-hash-link';
 
 function Nav() {
-    return (
-        <nav className='menu'>
-            <li className='menu-list'>
-                <HashLink to='#home' smooth={true} duration={300}><ul className='menu-item'>Accueil</ul></HashLink>
-                <HashLink to='#about' smooth={true} duration={300}><ul className='menu-item'>A propos</ul></HashLink>
-                <HashLink to='#projects' smooth={true} duration={300}><ul className='menu-item'>Projets</ul></HashLink>
-                <HashLink to='#contact' smooth={true} duration={300}><ul className='menu-item'>Contact</ul></HashLink>
-            </li>
-        </nav>
-    )
+  return (
+    <section class="top-nav">
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+<li className='menu-item'>
+          <HashLink to='#home' smooth={true} duration={300}>
+            Accueil
+          </HashLink>
+        </li>
+        <li className='menu-item'>
+          <HashLink to='#about' smooth={true} duration={300}>
+            A propos
+          </HashLink>
+        </li>
+        <li className='menu-item'>
+          <HashLink to='#projects' smooth={true} duration={300}>
+            Projets
+          </HashLink>
+        </li>
+        <li className='menu-item'>
+          <HashLink to='#contact' smooth={true} duration={300}>
+            Contact
+          </HashLink>
+        </li>
+    </ul>
+      </section>
+  );
 }
 
-export default Nav
+export default Nav;
