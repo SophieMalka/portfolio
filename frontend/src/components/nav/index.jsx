9b9213b563b5d33f302f1c838650548ef69e81fd
SophieMalka <s.malka@live.fr>
2023-06-21 11:43:28 +0200

    close nav onclick mobile

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,36 +1,43 @@
+import React, { useState } from 'react';
 import './index.css';
 import { HashLink } from 'react-router-hash-link';
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleItemClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <section class="top-nav">
-    <input id="menu-toggle" type="checkbox" />
-    <label class='menu-button-container' for="menu-toggle">
-    <div class='menu-button'></div>
-  </label>
-    <ul class="menu">
-<li className='menu-item'>
-          <HashLink to='#home' smooth={true} duration={300}>
+    <section className="top-nav">
+      <input id="menu-toggle" type="checkbox" checked={isOpen} onChange={() => setIsOpen(!isOpen)} />
+      <label className="menu-button-container" htmlFor="menu-toggle">
+        <div className="menu-button"></div>
+      </label>
+      <ul className={`menu ${isOpen ? 'open' : ''}`}>
+        <li className="menu-item">
+          <HashLink to="#home" smooth={true} duration={300} onClick={handleItemClick}>
             Accueil
           </HashLink>
         </li>
-        <li className='menu-item'>
-          <HashLink to='#about' smooth={true} duration={300}>
+        <li className="menu-item">
+          <HashLink to="#about" smooth={true} duration={300} onClick={handleItemClick}>
             A propos
           </HashLink>
         </li>
-        <li className='menu-item'>
-          <HashLink to='#projects' smooth={true} duration={300}>
+        <li className="menu-item">
+          <HashLink to="#projects" smooth={true} duration={300} onClick={handleItemClick}>
             Projets
           </HashLink>
         </li>
-        <li className='menu-item'>
-          <HashLink to='#contact' smooth={true} duration={300}>
+        <li className="menu-item">
+          <HashLink to="#contact" smooth={true} duration={300} onClick={handleItemClick}>
             Contact
           </HashLink>
         </li>
-    </ul>
-      </section>
+      </ul>
+    </section>
   );
 }
 
